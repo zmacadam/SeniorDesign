7ef5fa6c18fb2058e5e32cb9c9a95649e521c344
sorxrob <sorianorobertc@gmail.com>
2020-02-05 17:41:07 +0800

    [refactor] Force reload user when new content is available

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,8 @@
 module.exports = {
   transpileDependencies: ['vuetify'],
-  chainWebpack: config => {
-    config.module
-      .rule('eslint')
-      .use('eslint-loader')
-      .options({
-        fix: true
-      });
-  }
+  pwa: {
+    workboxOptions: {
+      skipWaiting: true,
+    },
+  },
 };
