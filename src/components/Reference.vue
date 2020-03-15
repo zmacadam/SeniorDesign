d62dc72ff7a52e59738be3e6acf5645c645999fe
Robert Anthonie Soriano <sorianorobertc@gmail.com>
2020-03-15 22:31:44 +0800

    [refactor] Add social share button

--- src/components/Reference.vue
+++ src/components/Reference.vue
@@ -1,25 +1,52 @@
 <template>
-  <v-card tile flat>
-    <v-card-text>
-      <div class="text-right">
-        Reference: <br />
-        <a
-          class="no-decoration"
-          href="https://bnonews.com/index.php/2020/01/timeline-coronavirus-epidemic/"
-          target="_BLANK"
-          >bnonews.com</a
-        >
-        <br />
-        <a class="no-decoration" href="https://github.com/CSSEGISandData/COVID-19" target="_BLANK"
-          >JHU CSSE</a
-        >
-        <br />
-        <a class="no-decoration" href="https://www.worldometers.info/coronavirus/" target="_BLANK"
-          >worldometers.info</a
-        >
-      </div>
-    </v-card-text>
-  </v-card>
+  <div>
+    <v-card tile flat>
+      <v-card-text class="d-flex justify-space-between">
+        <v-row>
+          <v-col
+            cols="12"
+            md="6"
+            :class="[$vuetify.breakpoint.mdAndUp ? 'text-left' : 'text-center']"
+          >
+            References:
+            <a
+              class="no-decoration"
+              href="https://bnonews.com/index.php/2020/01/timeline-coronavirus-epidemic/"
+              target="_BLANK"
+              >bnonews.com</a
+            >
+            -
+            <a
+              class="no-decoration"
+              href="https://github.com/CSSEGISandData/COVID-19"
+              target="_BLANK"
+              >JHU CSSE</a
+            >
+            -
+            <a
+              class="no-decoration"
+              href="https://www.worldometers.info/coronavirus/"
+              target="_BLANK"
+              >worldometers.info</a
+            >
+          </v-col>
+          <v-col
+            cols="12"
+            md="6"
+            :class="[$vuetify.breakpoint.mdAndUp ? 'text-right' : 'text-center']"
+          >
+            <a @click.prevent="share" class="white--text no-decoration">Share</a>
+          </v-col>
+        </v-row>
+      </v-card-text>
+    </v-card>
+    <v-snackbar bottom v-model="snackbar.show">
+      {{ snackbar.text }}
+      <v-btn color="red" text @click="snackbar.show = false">
+        Close
+      </v-btn>
+    </v-snackbar>
+  </div>
 </template>
 
 <script>
@@ -28,6 +55,32 @@ import { mapState } from 'vuex';
 export default {
   computed: {
     ...mapState(['isDarkTheme'])
+  },
+  data: () => ({
+    snackbar: {
+      show: false,
+      text: null
+    }
+  }),
+  methods: {
+    async share() {
+      if (navigator.share) {
+        try {
+          await navigator.share({
+            title: 'the2019ncov.com',
+            text: 'Map, data and timeline of coronavirus (COVID-19)',
+            url: 'https://the2019ncov.com'
+          });
+          this.snackbar.text = 'Shared successfully.';
+        } catch (e) {
+          this.snackbar.text = 'An error occurred.';
+        }
+      } else {
+        this.snackbar.text = 'Unsupported.';
+      }
+
+      this.snackbar.show = true;
+    }
   }
 };
 </script>
