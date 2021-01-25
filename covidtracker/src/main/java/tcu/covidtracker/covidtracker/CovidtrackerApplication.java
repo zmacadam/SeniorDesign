31e36218038eeedc2a12b71f0df7d4533f30270c
Zach Macadam <z.macadam@tcu.edu>
2021-01-25 11:57:10 -0600

    Created project.

---
+++ covidtracker/src/main/java/tcu/covidtracker/covidtracker/CovidtrackerApplication.java
@@ -0,0 +1,13 @@
+package tcu.covidtracker.covidtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidtrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CovidtrackerApplication.class, args);
+    }
+
+}
---
+++ covidtracker/src/test/java/tcu/covidtracker/covidtracker/CovidtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package tcu.covidtracker.covidtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidtrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
