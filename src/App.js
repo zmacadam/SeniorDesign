863643b81be5aa0000e37521741638af194c2186
damonram <66336019+damonram@users.noreply.github.com>
2021-03-28 21:40:59 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React, {useEffect, useState} from 'react';
-import {Cards, Chart, USMap} from './components';
+import {Info, Cond, Chart, USMap} from './components';
+//import {{Info, Cond}, Chart, USMap} from './components';
 import {fetchAllStatesByDate, fetchData} from './api/';
 import './App.css'
 import styles from './App.module.css';
-import Calendar from "react-calendar";
-import '../src/components/Calendar/Calendar.css';
-import moment from 'moment';
+//import {Info, Cond} from './components/Cards/Cards.jsx';
+//import getCond from './components/Cards/Cards.jsx';
+
 
 const App = () => {
     const [data, setData] = useState(null);
     const [statedata, setMapData] = useState(null);
     let today = new Date();
     let test = new Date();
+    let dd = String(today.getDate()).padStart(2, '0');
     let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
     let yyyy = today.getFullYear();
+    today = yyyy + '-' + mm + '-' + dd;
     test = yyyy + '-' + mm + '-' + 22;
-    const [dateState, setDateState] = useState(new Date())
-    const changeDate = (e) => {
-        setDateState(e)
-    }
     let ds = 'cases';
+//    let ds = Info.cond;
+    console.log(Cond.cond);
+
+
      useEffect(() => {
         let datas,data3;
         async function updatedata() {
@@ -27,26 +30,26 @@ const App = () => {
             data3 = await fetchAllStatesByDate(test);
             setData(data => datas);
             setMapData(statedata => data3);
+            // setCond(cond => ds);
+            // setTimeout(() => {
+            //     setMapData(statedata => data3);
+            // }, 3000);
+            // console.log(data3.length);
+            // setData(data => datas);
+            // setMapData(statedata => data3);
+            // console.log(statedata.length);
         }
         updatedata();
     }, []);
 
     return (
         <div className={styles.container}>
-            {data && <Cards data={data}/>}
-            <>
-                <Calendar
-                    value={dateState}
-                    onChange={changeDate}
-                />
-                <p>Current selected date is <b>{moment(dateState).format('YYYY-MM-DD')}</b></p>
-            </>
+            {data && <Info data={data}/>}
             <div className={styles.maps}>
                     {data && statedata && <svg width="960" height="600">
-                        <USMap date={moment(dateState).format('YYYY-MM-DD')} statedata={statedata} cond={ds}/>
+                        <USMap statedata={statedata} cond={ds}/>
                 </svg>}
             </div>
-            <br/>
             {data && <Chart data={data} country="US"/>}
         </div>
     );
