3ed74a183d62b9b9a5d3987bdbcd01147911ad80
damonram <66336019+damonram@users.noreply.github.com>
2021-04-19 01:40:33 -0500

    Add files via upload

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -12,8 +12,45 @@ import styles from './Cards.module.css';
 // const Info = ({ data: { confirmed, recovered, deaths, vaccinations, hospitalizations, lastUpdate } }) => {
 const Info = ({data: {cases, deaths, hospitalized, hospitalizedCum, hospitalizedCur, hospitalizedInc,newCases, newDeaths, peopleVaccinated,totalVaccinations,vaccinesDistributed}})=> {
   if (!cases) {
-    //console.log('load');
+    console.log('load');
     return 'Loading...';
+  }
+
+  if(hospitalized < 1){
+  return (
+  <div className={styles.container}>
+          {/*<Typography gutterBottom variant="h4" component="h2">Covid Tracker</Typography>*/}
+        <Grid container spacing={1} justify="center">
+          <CardComponent
+            className={styles.infected}
+            cardTitle="Cases"
+            value={cases}
+            cardSubtitle="Total number of active cases"
+          />
+          <CardComponent
+            className={styles.recovered}
+            cardTitle="New Cases"
+            value={newCases}
+            cardSubtitle="Total number of new cases"
+          />
+          <CardComponent
+            className={styles.deaths}
+            cardTitle="Deaths"
+            value={deaths}
+            cardSubtitle="Total number of deaths"
+          />
+          <CardComponent
+              className={styles.vaccinated}
+              cardTitle="Vaccinations"
+              value={peopleVaccinated}
+              cardSubtitle="Total number of vaccinations"
+
+          />
+
+
+        </Grid>
+      </div>
+      );
   }
 
   // let whichButton = null;
@@ -30,42 +67,31 @@ const Info = ({data: {cases, deaths, hospitalized, hospitalizedCum, hospitalized
           cardTitle="Cases"
           value={cases}
           cardSubtitle="Total number of active cases"
-//           buttonTitle="Infected"
-//           buttFunction= { (cond) => buttClickInfected() }
         />
         <CardComponent
           className={styles.recovered}
           cardTitle="New Cases"
           value={newCases}
           cardSubtitle="Total number of new cases"
-//           buttonTitle="Recovered"
-//           buttFunction= { (cond) => buttClickRecovered() }
         />
         <CardComponent
           className={styles.deaths}
           cardTitle="Deaths"
           value={deaths}
           cardSubtitle="Total number of deaths"
-//           buttonTitle="Deaths"
-//           buttFunction= { (cond) => buttClickDeaths() }
         />
         <CardComponent
             className={styles.vaccinated}
             cardTitle="Vaccinations"
             value={peopleVaccinated}
-//             value={0}
             cardSubtitle="Total number of vaccinations"
-//             buttonTitle="Vaccinations"
-//             buttFunction= {(cond) => buttClickVacc()}
+
         />
         <CardComponent
             className={styles.hospitalized}
             cardTitle="Hospitalizations"
             value={hospitalized}
-//             value={0}
             cardSubtitle="Total number of hospitalizations"
-//             buttonTitle="Hospitalizations"
-//             buttFunction= {(cond) => buttClickHosp()}
         />
 
       </Grid>
@@ -75,13 +101,13 @@ const Info = ({data: {cases, deaths, hospitalized, hospitalizedCum, hospitalized
 };
 
 // const Cond = function() {
-// //     //console.log(cond);
+// //     console.log(cond);
 //     return cond;
 // };
 
 // const Cond = getCond();
 //
-// // //console.log(Cond.cond);
+// // console.log(Cond.cond);
 // function getCond() {
 //     return cond
 // }
@@ -92,10 +118,10 @@ const Info = ({data: {cases, deaths, hospitalized, hospitalizedCum, hospitalized
 //
 // function buttClickInfected() {
 // //     document.getElementById("states").style.fill = 'blue';
-// //     //console.log("cases");
+// //     console.log("cases");
 //     cond = 'cases';
 //     setCond(cond);
-//     //console.log(cond);
+//     console.log(cond);
 // //     let stateColor = 'blue';
 //
 //     return(cond)
