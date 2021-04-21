bb8bdc9087e24cd15489224152d09a65dcb8aa82
nmbell <72823625+Nykolas7864@users.noreply.github.com>
2021-04-20 23:47:47 -0500

    Updates to Infos and About pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,48 +1,56 @@
-import React from "react";
-import "../App.css";
+import React from 'react';
+import '../App.css';
+import Spread from '../img/spread.png';
+import cuong from '../img/cuong.png';
+import zach from '../img/zach.png';
+import nick from '../img/nick.png';
+import joe from '../img/joe.png';
+import damon from '../img/damon.png';
 
-const About = () => {
-  return (
+const About = () => (
+  <div
+    style={{
+      backgroundColor: 'white',
+      textAlign: 'center',
+    }}
+  >
+    <br />
+    <h2>About Us</h2>
+    <br />
+    <img src={cuong} style={{ height: 250, width: 250 }} /> <img src={zach} style={{ height: 250, width: 250 }} /> <img src={nick} style={{ height: 250, width: 250 }} /> <img src={damon} style={{ height: 250, width: 250 }} /> <img src={joe} style={{ height: 250, width: 250 }} />
+    <p>
+        Our Team Members: Cuong Nguyen, Zach Macadam,  Nicholas Bell, Damon Ramirez, Joe Donoghue
+    </p>
+    <br />
+    <br />
     <div
       style={{
-        backgroundColor: "white",
-        textAlign: "center",
+        padding: '20px',
+        backgroundColor: 'grey',
+        color: 'white',
+        margin: '20px',
+        borderRadius: '20px',
       }}
     >
+      <h4>We are Covid Tracker Team</h4>
+      <div style={{ fontSize: '20px' }}>
+        Our team aims to support users in the United States by providing accurate and timely information regarding COVID-19. We understand this issue concerns everybody in the US right now, and that’s why our primary goal is to create software that emphasizes user-friendliness and reliability. Information this important needs to be accessible for all, and that knowledge allows us to make wiser decisions. To achieve this, our software features a web dashboard which displays an interactive map of the US, along with supplementary analysis and additional news.
+      </div>
       <br />
-      <h2>About Us</h2>
-      <br />
-      <br />
-      <br />
-      <div
-        style={{
-          padding: "20px",
-          backgroundColor: "grey",
-          color: "white",
-          margin: "20px",
-          borderRadius: "20px",
-        }}
-      >
-        <h4>We are Covid Tracker Team</h4>
-        <div style={{ fontSize: "20px" }}>
-          Our team aims to support users in the United States by providing accurate and timely information regarding COVID-19. We understand this issue concerns everybody in the US right now, and that’s why our primary goal is to create software that emphasizes user-friendliness and reliability. Information this important needs to be accessible for all, and that knowledge allows us to make wiser decisions. To achieve this, our software features a web dashboard which displays an interactive map of the US, along with supplementary analysis and additional news.
-        </div>
-        <br />
-          <h4>Want to know more about us?</h4>
-          <div style={{ fontSize: "20px" }}>
-              {" "}
-              <a
-                  href="http://riogrande.cs.tcu.edu/2021CovidTracker/"
-                  target="_blank"
-              >
-                  Click here
-              </a>{" "}
-          </div>
-        <br />
+      <h4>Want to know more about us?</h4>
+      <div style={{ fontSize: '20px' }}>
+        {' '}
+        <a
+          href="http://riogrande.cs.tcu.edu/2021CovidTracker/"
+          target="_blank"
+        >
+          Click here
+        </a>{' '}
       </div>
       <br />
     </div>
-  );
-}
+    <br />
+  </div>
+);
 
 export default About;
--- src/pages/Infos.js
+++ src/pages/Infos.js
@@ -25,18 +25,23 @@ const Symptom = () => {
             Here are some helpful tips for using this Covid Tracker:
           </p>
           <hr />
+          <br />
           <p>
             Begin by clicking on the date displayed on the home page. This will display a calendar for you to select what day you would like to see data for.
             The most recently available day will always be yesterday because it takes some time for public data sources to update.
-            By default, the home page will load data for the most recent day.
+            By default, the home page will load data for the most recent day. NOTE: The oldest data begins at January 21st, 2020.
           </p>
+          <hr />
+          <br />
           <p>
             There are green buttons at the top of the page that correspond to different types of data.
             Click any of the buttons to modify the map and graphs according to the data you would like to display.
             For visual clarity, each data type uses a unique color theme.
-            Note: The data source for hospitalization data has been discontinued, and that button is not available after March 7th 2021.
+            NOTE: The data source for hospitalization data has been discontinued, and that button is not available after March 7th 2021.
             Similarly, publicly available vaccine data began on January 12th, and therefore is not available before then.
           </p>
+          <hr />
+          <br />
           <p>
             Once you select the date and data you would like to see, please scroll across our interactive map of the U.S.!
             The map begins in the U.S. view.
@@ -46,41 +51,68 @@ const Symptom = () => {
             If you click on a county, the graphs and news will update relative to the county that was selected.
             The map displays a legend with a choropleth effect that shades states and counties based on the percentage of people affected in those areas relative to its population.
           </p>
+          <hr />
+          <br />
           <p>
             There is also a graph on the home page that displays data for the entire U.S.!
             All graphs provide the option to view trends for the last week, month, or year based on the date selected.
           </p>
+          <hr />
+          <br />
+          <p>
+            The News tab queries articles related to Covid in the state/county that is selected.
+            The date selected determines what date the most recent article must be published by to be shown.
+            NOTE: News articles older than a month before the present date will not be returned.
+          </p>
+          <hr />
+          <br />
+          <p>
+            Finally, we have a search bar that allows users to quickly find the information they are interested in.
+            Begin typing a state or county name to narrow down the search bar to a list of options that when chosen will handle selecting the data options for you.
+            This feature is especially useful if you know the name of a state or county but not the location of it on the map.
+          </p>
         </TabPanel>
         <TabPanel>
           <p>
-            Note: All data types display stats relative to the currently selected day and map region being viewed, and update accordingly
+            NOTE: All data types display stats relative to the currently selected day and map region being viewed, and update accordingly.
+            For dates when either Hospitalization or Vaccination data is not available, the button will be replaced with an asterisk to indicate this is the reason.
+            Also, these two data types were not provided for county data, and indicate that within the map.
           </p>
           <hr />
+          <br />
           <p>
             Cases: Total cases of Covid-19 up to the selected date within the current region (including both confirmed and probable).
           </p>
+          <br />
           <p>
             New Cases: Total new cases of Covid-19 recorded in just the current day selected.
           </p>
+          <br />
           <p>
             Deaths: Total deaths from Covid-19 up the selected date (including both confirmed and probable).
           </p>
+          <br />
           <p>
             Hospitalizations: Total number of people hospitalized due to Covid-19.
           </p>
+          <br />
           <p>
             Vaccinations: Total number of people who are documented as fully vaccinated (having received second or final dose).
-            Our vaccination data has multiple stats which are all displayed in the vaccination graphs.
+            Our vaccination data contains multiple stats which are all displayed in the vaccination graphs.
           </p>
+          <br />
           <p>
             Vaccines Distributed: Total number of all vaccines delivered.
           </p>
+          <br />
           <p>
             Daily Vaccinations: Total number of people who were vaccinated that day.
           </p>
+          <br />
           <p>
             Second Dose: Total number of people who have received their second or last vaccine dose.
           </p>
+          <br />
           <p>
             First Dose: Total number of people who have received the first dose of their vaccine.
           </p>
@@ -90,6 +122,7 @@ const Symptom = () => {
             DATA SOURCES
           </p>
           <hr />
+          <br />
           <ul>
             <li>The New York times: https://github.com/nytimes/covid-19-data</li>
             <li>Our World in Data: https://github.com/owid/covid-19-data</li>
